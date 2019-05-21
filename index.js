59405dfa195cbf0de3efdab3ba128158d08b66fb
toto <toto6321@qq.com>
2019-05-21 23:59:07 +0900

    FEATURE: add dotenv to isolate environment variables

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 'use strict';
 
-const app = require('./src/app.js')
+// load configuration
+const configuration = require('dotenv').config({path: 'config.env'})
+console.log(configuration)
 
-const port = 3000
+// get environment variables
+const env = process.env.NODE_ENV || 'development'
+const port = process.env.NODE_PORT || '3000'
+const src = env === 'production' ? './build' : './src'
+
+const app = require(src + '/app.js')
+
 app.listen(port)
 
 console.log(`app is listening on ${port}`)
