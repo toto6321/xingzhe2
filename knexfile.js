cec88328eaf474678ed89a44163bd183236a83b1
toto <toto6321@qq.com>
2019-05-22 19:11:19 +0800

    FEATURE: create table user

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,7 @@
 // Update with your config settings.
 
+require('dotenv').config({path: 'config.env'})
+
 module.exports = {
   development: {
     client: 'mysql',
---
+++ src/db/migrations/dev/20190522190328_create_table_user.js
@@ -0,0 +1,20 @@
+exports.up = function (knex, Promise) {
+  return knex.schema.raw("CREATE TABLE IF NOT EXISTS `user` (\n" +
+    "\t`id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,\n" +
+    "\t`nickname` VARCHAR(50) NOT NULL DEFAULT '',\n" +
+    "\t`email` VARCHAR(100) NOT NULL DEFAULT '',\n" +
+    "\t`tel_code` VARCHAR(10) NOT NULL DEFAULT '86',\n" +
+    "\t`telephone` VARCHAR(20) NOT NULL DEFAULT '',\n" +
+    "\t`password` VARCHAR(100) NOT NULL DEFAULT '',\n" +
+    "\t`token` VARCHAR(100) NOT NULL DEFAULT '',\n" +
+    "\t`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,\n" +
+    "\t`updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,\n" +
+    "\tPRIMARY KEY(`id`)\n" +
+    ") ENGINE = InnoDB COMMENT 'table for user info';\n")
+};
+
+exports.down = function (knex, Promise) {
+  if (process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
+    return knex.schema.dropTableIfExists('user')
+  }
+};
