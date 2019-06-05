8e24a1f2a4095681bf686a83df0a3b2c50f1bccd
toto <toto6321@qq.com>
2019-06-05 11:15:08 +0800

    FEATURE: add seed file with faker package

---
+++ src/db/seeds/dev/user.js
@@ -0,0 +1,42 @@
+if (!process.env.NODE_ENV) {
+  throw new Error(`NODE_ENV is unset!`)
+}
+
+if (process.env.NODE_ENV !== 'development') {
+  throw new Error(`Seed is only allowed in development!`)
+}
+
+const faker = require('faker')
+
+const seed = async knex => {
+  let mock_data = [{
+    nickname: 'demo',
+    email: 'demo@xingzhe.com',
+    tel_code: '86',
+    telephone: '12345678911',
+    password: 'password',
+    token: 'token'
+  }]
+
+  // faker.setLocale('zh_CN')
+
+  for (let i = 0; i < 9; i++) {
+    let user = {
+      nickname: faker.internet.userName(),
+      email: faker.internet.email(),
+      tel_code: '86',
+      telephone: faker.phone.phoneNumber().replace('-', ''),
+      password: faker.internet.password(),
+      token: faker.internet.password()
+    }
+    mock_data.push(user)
+  }
+
+  // delete all data there
+  await knex('user').truncate()
+
+  // insert mock data
+  await knex('user').insert(mock_data)
+}
+
+module.exports = {seed}
