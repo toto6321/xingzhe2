5b6eed23233ef043504b78aee96653befd1f4cd7
toto <toto6321@qq.com>
2019-06-05 11:13:24 +0800

    FIX: fix the wrong usage of knex constructor

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,5 @@
 const env = process.env.NODE_ENV || 'development'
 
 const db_config = require('../../knexfile')
-module.exports = require('knex')[db_config[env]]
+module.exports = require('knex')(db_config[env])
 
