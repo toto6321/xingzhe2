b53385b116a0a865ffe4845e52ea31cdd83abdea
toto <toto6321@qq.com>
2019-06-06 18:07:51 +0800

    FIX: fix a problem that controller doesn't immediately send the response when input is sufficient

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,13 +1,16 @@
 'use strict'
 const userService = require('../services/user/userServices')
-const login = (ctx, next) => {
+const login = ctx => {
   const token = get_jwt_token(ctx)
   ctx.body = { token }
 }
 
-const signup = async (ctx, next) => {
+const signup = async ctx => {
   const { email, code, phone } = ctx.request.body
-  if (!email || !phone) ctx.status = 412
+  if (!email || !phone) {
+    ctx.status = 412
+    return
+  }
   const result = await userService.is_email_existed(email)
   const data = await userService.get_many_by_telephone(code, phone)
   if (!result || data.length > 0) {
@@ -17,14 +20,14 @@ const signup = async (ctx, next) => {
   }
 }
 
-const info = async (ctx, next) => {
+const info = async ctx => {
   let { code, phone } = ctx.request.body
   const data = await userService.get_many_by_telephone(code, phone)
   ctx.body = { data }
 }
 
-const get_jwt_token = (ctx) => {
-  // return ctx.request.header['authorization']
+const get_jwt_token = ctx => {
+  return ctx.request.header['authorization']
 }
 
 module.exports = {
