a347a31e354a4f8f99525e21f664f251aeb95693
toto <toto6321@qq.com>
2019-06-12 11:40:46 +0800

    REFACTOR: refactor error middleware to record the log

--- src/middlewares/error/index.js
+++ src/middlewares/error/index.js
@@ -1,8 +1,11 @@
 const errorHandler = require('koa-json-error')
 const _ = require('lodash')
+const logger = require('../../middlewares/log/init')
 
 const postFormat = (err0, err1) => {
-  return _.omit(_.assign(err0, err1), 'stack')
+  const error = _.omit(_.assign(err0, err1), 'stack')
+  logger.warn(error)
+  return error
 }
 
 module.exports = errorHandler(postFormat)
